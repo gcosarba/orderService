8aa500fd05a22b3402f733a8334b70c42d5dfd03
GeorgeCosarba <gcosarba@brightcove.com>
2020-07-07 20:53:25 -0700

    Basic spring web app

---
+++ src/main/java/com/gcosarba/orders/SimpleOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.gcosarba.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOrdersApplication.class, args);
+	}
+
+}
